2fc083454a93e99116f67fd1652d660c6d5308f8
Ângelo Gabriel <angelobielmoreira@hotmail.com>
2024-05-15 03:13:28 -0300

    feat: create 2 new docker-composes to run multiple instances of wordpress separately and adjust the nginx.conf

--- locustfile.py
+++ locustfile.py
@@ -1,5 +1,6 @@
 from locust import HttpUser, task, between
 
+
 class WebsiteUser(HttpUser):
     wait_time = between(1, 5)
 
@@ -7,12 +8,8 @@ class WebsiteUser(HttpUser):
     def index_page(self):
         self.client.get("/?p=1")
 
-    @task(3)
-    def view_posts(self):
-        for post_id in range(10):
-            self.client.get(f"/?p={post_id}", name="/?p=[id]")
-            self.wait()
-
     @task(2)
     def specific_post(self):
-        self.client.get("/2024/05/06/teste/")
+        self.client.get("/2024/05/10/teste/")  # 2mb images
+        self.client.get("/2024/05/11/teste2/")  # 400kb texto
+        self.client.get("/2024/05/11/teste3/")  # 300kb image

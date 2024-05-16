450133236e9f13913a2b581602545266ea4dffb9
lucaheset <lucaheset77@gmail.com>
2024-05-15 21:09:36 -0300

    add csv

--- locustfile.py
+++ locustfile.py
@@ -10,6 +10,9 @@ class WebsiteUser(HttpUser):
 
     @task(2)
     def specific_post(self):
-        self.client.get("/2024/05/10/teste/")  # 2mb images
-        self.client.get("/2024/05/11/teste2/")  # 400kb texto
-        self.client.get("/2024/05/11/teste3/")  # 300kb image
+        # self.client.get("/2024/05/10/teste/")  # 2mb images
+        self.client.get("/2024/05/06/teste/")  # 2mb images
+        # self.client.get("/2024/05/11/teste2/")  # 400kb texto
+        self.client.get("/2024/05/15/teste2/")  # 400kb texto
+        # self.client.get("/2024/05/11/teste3/")  # 300kb image
+        self.client.get("/2024/05/15/teste3/")  # 300kb image
